61c1f1426b62b5427554a1e7245f7113b2d07e28
MoutasemQlesha <newdim70@gmail.com>
2025-07-06 11:29:49 +0300

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,14 @@
+
+setTimeout(function () {
+    $(".zcp-col").each(function () {
+        if (!$(this).hasClass("custom-cart")) {
+            $(this).addClass("custom-cart");
+            $(this).append(`
+                <div style="margin-top:10px">
+                    <input type="number" placeholder="Qty" style="width:50px;" />
+                    <button onclick="alert('Added to cart')" style="margin-left:5px;">Add</button>
+                </div>
+            `);
+        }
+    });
+}, 3000);
